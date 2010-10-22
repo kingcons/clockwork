d455bf456f27af1621614a1b746e00afd963b529
Brit Butler <redline6561@gmail.com>
2010-10-21 22:35:10 -0400

    Implemented schedule-reminder method, split out defview and form logic into a separate file and started on :on-success callback.

---
+++ src/forms.lisp
@@ -0,0 +1,87 @@
+(in-package :clockwork)
+
+(defparameter *timezones*
+  '(("UTC-12:00" . -43200) ; Eniwetok, Kwajalein
+    ("UTC-11:00" . -39600) ; Midway Island, Samoa
+    ("UTC-10:00" . -36000) ; Hawaii
+    ("UTC-09:00" . -32400) ; Alaska
+    ("UTC-08:00" . -28800) ; Pacific Time (US and Canada)
+    ("UTC-07:00" . -25200) ; Mountain Time (US and Canada)
+    ("UTC-06:00" . -21600) ; Central Time (US and Canada)
+    ("UTC-05:00" . -18000) ; Eastern Time (US and Canada)
+    ("UTC-04:00" . -14400) ; Atlantic Time (Canada), Caracas, La Paz
+    ("UTC-03:30" . -12600) ; Newfoundland
+    ("UTC-03:00" . -10800) ; Brazil, Buenos Aires, Georgetown
+    ("UTC-02:00" . -7200) ; Mid-Atlantic
+    ("UTC-01:00" . -3600) ; Azores, Cape Verde Islands
+    ("UTC+00:00" . 0) ; London, Lisbon, Casablanca
+    ("UTC+01:00" . 3600) ; Berlin, Brussels, Copenhagen, Madrid, Paris
+    ("UTC+02:00" . 7200) ; Kaliningrad, South Africa
+    ("UTC+03:00" . 10800) ; Baghdad, Riyadh, Moscow, St. Petersburg
+    ("UTC+03:30" . 12600) ; Tehran
+    ("UTC+04:00" . 14400) ; Abu Dhabi, Muscat, Baku, Tbilisi
+    ("UTC+04:30" . 16200) ; Kabul
+    ("UTC+05:00" . 18000) ; Ekaterinburg, Islamabad, Karachi, Tashkent
+    ("UTC+05:30" . 19800) ; Bombay, Calcutta, Madras, New Delhi
+    ("UTC+05:45" . 20700) ; Kathmandu
+    ("UTC+06:00" . 21600) ; Almaty, Dhaka, Colombo
+    ("UTC+07:00" . 25200) ; Bangkok, Hanoi, Jakarta
+    ("UTC+08:00" . 28800) ; Beijing, Perth, Singapore, Hong Kong
+    ("UTC+09:00" . 32400) ; Tokyo, Seoul, Osaka, Sapporo, Yakutsk
+    ("UTC+09:30" . 34200) ; Adelaide, Darwin
+    ("UTC+10:00" . 36000) ; Eastern Australia, Guam, Vladivostok
+    ("UTC+11:00" . 39600) ; Magadan, Solomon Islands, New Caledonia
+    ("UTC+12:00". 43200))) ; Auckland, Wellington, Fiji, Kamchatka
+
+(defparameter *hour-choices*
+  (loop for i from 0 to 23
+     collecting `(,(format nil "~d" i) . ,i)))
+
+(defview reminder-form-view (:type form :caption "Schedule an Event Reminder..."
+			     :buttons '((:submit . "Submit")))
+  (send-as :present-as (dropdown :choices '(("An email and a text." . :both)
+					    ("Just an e-mail." . :email)
+					    ("Just a text." . :text))
+				 :welcome-name "How to send it")
+	   :requiredp t)
+  (email)
+  (cell-number)
+  (cell-carrier :present-as (dropdown :choices *sms-gateways*))
+  (event-date :present-as (calendar) :requiredp t)
+  (event-hour :present-as (dropdown :choices *hour-choices*)
+	      :requiredp t)
+  (event-minute :present-as (dropdown :choices   '(("00" . 0)
+						   ("15" . 15)
+						   ("30" . 30)
+						   ("45" . 45)))
+		:requiredp t)
+  (timezone :present-as (dropdown :choices *timezones*)
+	    :requiredp t)
+  (remind-me :present-as (dropdown :choices '(("At the event" . 0)
+					      ("15 minutes before" . 900)
+					      ("30 minutes before" . 1800)
+					      ("45 minutes before" . 2700)
+					      ("1 hour before" . 3600)
+					      ("2 hours before" . 7200)
+					      ("1 day before" . 86400)
+					      ("2 days before" . 172800)
+					      ("1 week before" . 604800)
+					      ("2 weeks before" . 1209600)))
+	     :requiredp t)
+  (subject :requiredp t)
+  (summary :present-as (textarea :rows 5))
+  (honeypot :label "Leave this blank"))
+
+(defun get-emails (form-data)
+  (with-slots (send-as email cell-number cell-carrier) form-data
+    (let ((sms-mail (concatenate 'string cell-number "@" cell-carrier)))
+      (ecase send-as
+	(:both (list email sms-mail))
+	(:email (list email))
+	(:text (list sms-mail))))))
+
+(defun get-reminder-time (form-data)
+  ())
+
+(defun get-event-time (form-data)
+  ())
--- src/init-session.lisp
+++ src/init-session.lisp
@@ -5,3 +5,23 @@
 (defun init-user-session (root)
   (setf (widget-children root)
 	(make-reminder-form)))
+
+(defun make-reminder-form ()
+  (make-instance 'dataform
+		 :data (make-instance (class-from-view 'reminder-form-view))
+		 :ui-state :form
+		 :form-view 'reminder-form-view
+		 :on-success
+		 (lambda (obj)
+		   (let ((new-reminder (create-reminder (dataform-data obj))))
+		     (schedule-reminder new-reminder)
+		     (persist-object *default-store* new-reminder)))))
+
+(defun create-reminder (form-data)
+  (with-slots (subject summary) form-data
+    (make-instance 'reminder
+		   :emails (get-emails form-data)
+		   :title subject
+		   :summary summary
+		   :timestamp (get-event-time form-data)
+		   :at (get-reminder-time form-data))))
