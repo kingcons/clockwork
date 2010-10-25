1daf23f713380880ee264c89ffd3081c3f92ad38
Brit Butler <redline6561@gmail.com>
2010-10-25 17:06:57 -0400

    A quick fixup to make-reminder-form and some TODO updates.

--- src/init-session.lisp
+++ src/init-session.lisp
@@ -5,26 +5,17 @@
   (setf (widget-children root)
 	(make-reminder-form)))
 
-;; new make-reminder-form
 (defun make-reminder-form ()
-  (let ((reminder-form (make-instance 'form-widget)))
-;				      :on-success #'submit-reminder-form)))
-    (form-widget-initialize-from-view reminder-form 'reminder-form-view)))
-
-;; (defun make-reminder-form ()
-;;   (make-instance 'dataform
-;; 		 :data (make-instance (class-from-view 'reminder-form-view))
-;; 		 :ui-state :form
-;; 		 :form-view 'reminder-form-view
-;; 		 :on-success (lambda (obj)
-;; 			       (setf *last-post* (dataform-data obj)))))
-		 ;; (lambda (obj)
-		 ;;   (let ((new-reminder (create-reminder (dataform-data obj))))
-		 ;;     (schedule-reminder new-reminder)
-		 ;;     (persist-object *default-store* new-reminder)))))
+  (let ((reminder-form (make-instance 'form-widget
+				      :on-success (lambda (obj)
+						    (setf *last-post* obj)))))
+    (form-widget-initialize-from-view reminder-form 'reminder-form-view)
+    reminder-form))
 
 (defun submit-reminder-form (obj)
-  (setf *last-post* obj))
+  (let ((new-reminder (create-reminder (dataform-data obj))))
+    (schedule-reminder new-reminder)
+    (persist-object *default-store* new-reminder)))
 
 (defun create-reminder (form-data)
   (with-slots (subject summary) form-data
